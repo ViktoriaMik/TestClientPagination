35d28be3d24df302c06100af61802924acf94a85
ViktoriaMik <victoriamik2015@gmail.com>
2021-10-23 13:02:30 +0300

    pagination fourth step

--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -7,7 +7,7 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 })
 export class PaginationComponent implements OnInit {
   pages = [1, 2, 3]
-  selectedPage: number =1
+  selectedPage: number = 1
   @Output()
   lift = new EventEmitter
 
@@ -17,15 +17,10 @@ export class PaginationComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  // catchPage(value: number) {
-  //   this.selectedPage = value
-  //   this.lift.emit(this.selectedPage)
-  // }
-
-  setSelectedpage(page:number){
+  setSelectedpage(page: number) {
     console.log(page)
-    this.selectedPage=page
-this.lift.emit(this.selectedPage)
+    this.selectedPage = page
+    this.lift.emit(this.selectedPage)
 
   }
 }
